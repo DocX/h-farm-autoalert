a32a3923e238131a427d5cf66a8a48a1011fd9c1
Michal Pawlowski <misza222@gmail.com>
2014-05-18 07:53:16 +0200

    Add button for doing work in a thread

--- src/com/autoalert/MainActivity.java
+++ src/com/autoalert/MainActivity.java
@@ -48,7 +48,23 @@ public class MainActivity extends ActionBarActivity {
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 	}
-
+	
+	public void startCollectionInBackground(View v) {
+	    new Thread(new Runnable() {
+	        public void run() {
+	        	for(int i = 0; i < 10; i++) {
+	        		try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+	        		Log.d("Testing", "another one");
+	        	}
+	        }
+	    }).start();
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
